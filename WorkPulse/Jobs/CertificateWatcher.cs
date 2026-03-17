dcc9741c45b6ff04a289b6457e8b57f2558ba553
Mikołaj Kocik <mikolajkocik@onet.pl>
2026-03-17 15:55:41 +0100

    feat: add background job with certificates alert

---
+++ WorkPulse/Jobs/CertificateWatcher.cs
@@ -0,0 +1,36 @@
+﻿using CertMonitor.API.Interfaces;
+using WorkPulse.Interfaces;
+
+namespace CertMonitor.API.Jobs;
+
+public sealed class CertificateWatcher : BackgroundService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public CertificateWatcher(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var certService = scope.ServiceProvider.GetRequiredService<ICertificateService>();
+                var systemService = scope.ServiceProvider.GetRequiredService<ISystemService>();
+
+                var certs = await certService.GetCertificatesAsync();
+                int criticalCount = certs.Count(c => c.ToExpire < 7 && c.IsActive);
+
+                if (criticalCount > 0)
+                {
+                    await systemService.RunHealthAsync();
+                }
+            } 
+
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+    }
+}
