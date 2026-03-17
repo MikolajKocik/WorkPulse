73201e0f35e5c79a313f888ae1b65bd1ae4e4b9f
Mikołaj Kocik <mikolajkocik@onet.pl>
2026-03-17 14:40:18 +0100

    merge: fix merge conflict in startup file and DI

--- WorkPulse/Program.cs
+++ WorkPulse/Program.cs
@@ -9,12 +9,8 @@ using MudBlazor.Services;
 using System.Net.Http.Headers;
 using System.Text;
 using WorkPulse.Components;
-<<<<<<< HEAD
 using WorkPulse.Interfaces;
-=======
->>>>>>> ee7705610140febc7ec3fdb84e521b4faabee139
 using WorkPulse.Services;
-using WorkPulse.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,10 +26,7 @@ builder.Services.AddScoped<IWorkItemService, WorkItemService>();
 builder.Services.AddScoped<IProfileService, ProfileService>();
 builder.Services.AddScoped<IAzureBoardService, AzureBoardService>();
 builder.Services.AddScoped<ICertificateService, CertificateService>();
-<<<<<<< HEAD
 builder.Services.AddScoped<ISystemService, SystemService>(); 
-=======
->>>>>>> ee7705610140febc7ec3fdb84e521b4faabee139
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IUserContext, UserContext>();
