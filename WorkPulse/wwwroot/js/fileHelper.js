5ce65776f82a75ca164aeb2d161339f4512343ec
Mikołaj Kocik <mikolajkocik@onet.pl>
2026-03-17 17:06:06 +0100

    refactor: replace global JS window function with ES module for file download

---
+++ WorkPulse/wwwroot/js/fileHelper.js
@@ -0,0 +1,12 @@
+﻿export async function downloadFile(fileName, content) {
+    const blob = new Blob([content], { type: 'text/csv;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = fileName ?? '';
+    anchor.click();
+    anchor.remove();
+
+    URL.revokeObjectURL(url);
+}
